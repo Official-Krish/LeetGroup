a1c22d7366bfd96036bcbbb1677b7f55e5473467
Official-Krish <krishanand0103@gmail.com>
2024-10-04 21:19:21 +0530

    build fix

--- app/api/ReloadStats/route.ts
+++ app/api/ReloadStats/route.ts
@@ -1,11 +1,38 @@
-export const dynamic = 'force-dynamic';
-
-import calculateDailyPerformance from '@/lib/dailyPerformance';
+import { fetchSolvedProblems } from '@/lib/dailyPerformance';
+import { prisma } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
 export const POST = async () => {
     try {
-        await calculateDailyPerformance();
+        const groups = await prisma.group.findMany({
+            include: {
+                members: true,
+            },
+        });
+        
+        for (const group of groups) {
+            for (const member of group.members) {
+            const solvedCount = await fetchSolvedProblems(member.leetcodeId); 
+        
+            await prisma.performance.upsert({
+                where: {
+                userId_groupId: {
+                    userId: member.id,
+                    groupId: group.groupId, 
+                },
+                },
+                update: {
+                solvedCount: solvedCount,
+                createdAt: new Date(),
+                },
+                create: {
+                userId: member.id,
+                groupId: group.groupId, 
+                solvedCount: solvedCount,
+                },
+            });
+            }
+        }
         return NextResponse.json({ message: 'Stats reloaded' });
     } catch (error) {
         console.error('Error fetching leaderboard:', error);
--- lib/dailyPerformance.ts
+++ lib/dailyPerformance.ts
@@ -1,42 +1,6 @@
 import axios from 'axios';
-import { prisma } from './db';
-
-
-const calculateDailyPerformance = async () => {
-  const groups = await prisma.group.findMany({
-    include: {
-      members: true,
-    },
-  });
-
-  for (const group of groups) {
-    for (const member of group.members) {
-      const solvedCount = await fetchSolvedProblems(member.leetcodeId); 
-
-      await prisma.performance.upsert({
-        where: {
-          userId_groupId: {
-            userId: member.id,
-            groupId: group.groupId, 
-          },
-        },
-        update: {
-          solvedCount: solvedCount,
-          createdAt: new Date(),
-        },
-        create: {
-          userId: member.id,
-          groupId: group.groupId, // Ensure this is the correct groupId type (string)
-          solvedCount: solvedCount,
-        },
-      });
-    }
-  }
-};
-
 export const fetchSolvedProblems = async (leetcodeId: string): Promise<number> => {
   const res = await axios.get(`https://leetcode-stats-api.herokuapp.com/${leetcodeId}`);
   return res.data.totalSolved;
 };
 
-export default calculateDailyPerformance;
